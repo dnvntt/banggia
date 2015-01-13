71696bd9050a813e1db3a27ae3760693c549886e
unknown <khanh.doan@ipa_d871.ipa.com.vn>
2015-01-13 18:19:08 +0700

    Change return for pingCheck

--- src/main/java/priceboard/rest/controller/PingController.java
+++ src/main/java/priceboard/rest/controller/PingController.java
@@ -1,9 +1,9 @@
 package priceboard.rest.controller;
 
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.jboss.logging.Param;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/")
 public class PingController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public @ResponseBody String checkPing() {
-		return "Ok";
+	public @ResponseBody String checkPing(@RequestParam("ping") String jqueryCallback) {
+		return jqueryCallback+"({})";
 	}
 }
